38d14c6581666f1bbe8a40d55e780012ac6b954e
AmmarSaadDine <ammar.as.89@gmail.com>
2016-08-15 16:05:02 +0200

    fix more paths & add push notifications support

--- scripts/app.js
+++ scripts/app.js
@@ -235,7 +235,11 @@
      console.log('Service Worker is supported');
    navigator.serviceWorker.register('service-worker.js').then(function(reg) {
      console.log(':^)', reg);
-     // TODO
+     reg.pushManager.subscribe({
+            userVisibleOnly: true
+        }).then(function(sub) {
+            console.log('endpoint:', sub.endpoint);
+        });
    }).catch(function(err) {
      console.log(':^(', err);
    });
