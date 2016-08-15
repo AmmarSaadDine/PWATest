e3ad42b8fe5da029a73844809ab736d24ead7b33
AmmarSaadDine <ammar.as.89@gmail.com>
2016-08-15 18:10:41 +0200

    change order of methods

--- scripts/app.js
+++ scripts/app.js
@@ -96,25 +96,6 @@
    *
    ****************************************************************************/
 
-  function subscribe() {
-    reg.pushManager.subscribe({userVisibleOnly: true}).
-    then(function(pushSubscription){
-      sub = pushSubscription;
-      console.log('Subscribed! Endpoint:', sub.endpoint);
-      subscribeButton.textContent = 'Unsubscribe';
-      isSubscribed = true;
-    });
-  }
-  function unsubscribe() {
-    sub.unsubscribe().then(function(event) {
-      subscribeButton.textContent = 'Subscribe';
-      console.log('Unsubscribed!', event);
-      isSubscribed = false;
-    }).catch(function(error) {
-      console.log('Error unsubscribing', error);
-      subscribeButton.textContent = 'Subscribe';
-    });
-  }
   // Toggles the visibility of the add new city dialog.
   app.toggleAddDialog = function(visible) {
     if (visible) {
@@ -276,4 +257,23 @@
     });
   }
 
+  function subscribe() {
+    reg.pushManager.subscribe({userVisibleOnly: true}).
+    then(function(pushSubscription){
+      sub = pushSubscription;
+      console.log('Subscribed! Endpoint:', sub.endpoint);
+      subscribeButton.textContent = 'Unsubscribe';
+      isSubscribed = true;
+    });
+  }
+  function unsubscribe() {
+    sub.unsubscribe().then(function(event) {
+      subscribeButton.textContent = 'Subscribe';
+      console.log('Unsubscribed!', event);
+      isSubscribed = false;
+    }).catch(function(error) {
+      console.log('Error unsubscribing', error);
+      subscribeButton.textContent = 'Subscribe';
+    });
+  }
 })();
